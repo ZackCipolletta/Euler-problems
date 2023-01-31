41ba89b35f5370793fe64d44e0531bf1a4c4eb33
Zachary Cipolletta <ZackCipolletta@gmail.com>
2023-01-31 13:03:21 -0800

    cleaned up white space and page formatting
    Co-authored-by: James Provance <jb24x7@gmail.com>

--- __tests__/triangle.test.js
+++ __tests__/triangle.test.js
@@ -1,19 +1,18 @@
 import { multiplesOf3and5, fibSequenceBelow4Mil } from "./../src/eulerProblems";
 
 
-// describe('multiplesOf3and5', () => {
+describe('multiplesOf3and5', () => {
 
-//     test('should return an array containing 3, 6 and 9', () => {
-//         multiplesOf3and5();
-//         expect(multiplesOf3and5()).toEqual(233168);
-//     });
-//   });
+  test('should return an array containing 3, 6 and 9', () => {
+    multiplesOf3and5();
+    expect(multiplesOf3and5()).toEqual(233168);
+  });
+});
 
 describe('fibSequenceBelow4Mil', () => {
 
   test('should return and array of Fibonacci numbers 1 to 3', () => {
     fibSequenceBelow4Mil();
-    expect(fibSequenceBelow4Mil()).toEqual(44);
+    expect(fibSequenceBelow4Mil()).toEqual(4613732);
   });
-
 });
--- src/eulerProblems.js
+++ src/eulerProblems.js
@@ -1,22 +1,19 @@
-// export function multiplesOf3and5() {
-//   let arr1 = [];
-//   for(let i = 3; i < 1000; i+=3) {
-//     arr1.push(i);
-//   }
-//   for(let i = 5; i < 1000; i+=5) {
-//     if(!arr1.includes(i)) {
-//       arr1.push(i);
-//     }
-
-//   }
-//   let x = 0;
-//   arr1.forEach(element =>{
-//     x += element;
-//   });
-//   console.log(arr1);
-//   return x;
-
-// }
+export function multiplesOf3and5() {
+  let arr1 = [];
+  for(let i = 3; i < 1000; i+=3) {
+    arr1.push(i);
+  }
+  for(let i = 5; i < 1000; i+=5) {
+    if(!arr1.includes(i)) {
+      arr1.push(i);
+    }
+  }
+  let x = 0;
+  arr1.forEach(element =>{
+    x += element;
+  });
+  return x;
+}
 
 
 export function fibSequenceBelow4Mil() {
@@ -30,11 +27,9 @@ export function fibSequenceBelow4Mil() {
       fibArray.push(fibArray[x - 2] + fibArray[x - 1]);
     }
   }
-
   fibArray.forEach(element => {
     if (Number.isInteger(element / 2))
       fibSum += element;
   });
-  console.log(fibArray);
   return fibSum;
 }
