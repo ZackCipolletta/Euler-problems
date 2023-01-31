fc6f26095c3b56018a7ceec9f3af171517163e54
Zachary Cipolletta <ZackCipolletta@gmail.com>
2023-01-31 15:24:27 -0800

    add functions to check if a given number is a palindrome and a function to multiply 100-110 by 100 and add them to an array.
    Co-authored-by: James Provance <jb24x7@gmail.com>

--- __tests__/triangle.test.js
+++ __tests__/triangle.test.js
@@ -1,4 +1,4 @@
-import { multiplesOf3and5, fibSequenceBelow4Mil } from "./../src/eulerProblems";
+import { multiplesOf3and5, fibSequenceBelow4Mil, palindromicNumbers, multiplyAllNumbers} from "./../src/eulerProblems";
 
 
 describe('multiplesOf3and5', () => {
@@ -14,5 +14,19 @@ describe('fibSequenceBelow4Mil', () => {
   test('should return and array of Fibonacci numbers 1 to 3', () => {
     fibSequenceBelow4Mil();
     expect(fibSequenceBelow4Mil()).toEqual(4613732);
+  });
+});
+
+describe('palindromicNumbers', () => {
+
+  test('should return a value of true or false depending on if a provided is a palindrome', () => {
+    expect(palindromicNumbers(900009)).toBeTruthy();
+  });
+});
+
+describe('multiplyAllNumbers', () => {
+
+  test('should return an array of the product of 100 and every number from 100 - 110', () => {
+    expect(multiplyAllNumbers()).toEqual([10000, 10100, 10200, 10300, 10400, 10500, 10600, 10700, 10800, 10900, 11000]);
   });
 });
--- src/eulerProblems.js
+++ src/eulerProblems.js
@@ -1,25 +1,24 @@
 export function multiplesOf3and5() {
   let arr1 = [];
-  for(let i = 3; i < 1000; i+=3) {
+  for (let i = 3; i < 1000; i += 3) {
     arr1.push(i);
   }
-  for(let i = 5; i < 1000; i+=5) {
-    if(!arr1.includes(i)) {
+  for (let i = 5; i < 1000; i += 5) {
+    if (!arr1.includes(i)) {
       arr1.push(i);
     }
   }
   let x = 0;
-  arr1.forEach(element =>{
+  arr1.forEach(element => {
     x += element;
   });
   return x;
 }
 
-
 export function fibSequenceBelow4Mil() {
   let fibArray = [1, 2, 3];
   let fibSum = 0;
-  while (fibArray[fibArray.length-1] < 4000000) {
+  while (fibArray[fibArray.length - 1] < 4000000) {
     let x = fibArray.length;
     if (fibArray[x - 2] + fibArray[x - 1] >= 4000000) {
       break;
@@ -33,3 +32,24 @@ export function fibSequenceBelow4Mil() {
   });
   return fibSum;
 }
+
+
+export function multiplyAllNumbers() {
+  let arrayForMultiply = [];
+  for (let i = 100; i <= 110; i++) {
+    arrayForMultiply.push(i * 100);
+  }
+
+  return arrayForMultiply;
+}
+
+export function palindromicNumbers(someNum) {
+  let numArray = someNum.toString().split('');
+  let x = numArray.length;
+  if (x === 5 && numArray[0] === numArray[x-1] && numArray[1] === numArray[x-2]) {
+    return true;
+  } else if (numArray.length === 6 && numArray[0] === numArray[x-1] && numArray[1] === numArray[x-2] && numArray[2] === numArray[x-3]) {
+    return true;
+  }
+}
+
